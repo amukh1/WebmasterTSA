bccfb5d46380ab3fbb6bb03cd533ea27c35355ae
Amukh1 <85003861+amukh1@users.noreply.github.com>
2024-11-14 16:49:41 +0000

    nav_bar_update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,20 @@ import {
     useLocation
 } from "react-router-dom";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, button } from "react";
 
 export function Nav(props) {
     return <div>
-        <div className="bar">froge</div>
+        <div className="bar"> 
+            <div className="bar1">
+                <div>LOGO</div>
+                <p>Menu</p>
+                <p>About Us</p>
+            </div>
+            <div className="bar2">
+                <button>Location</button>
+                <button>Contact</button>
+            </div>
+        </div>
     </div>
 }
