074785f46f3ac56dd19ebcd9d8119e844c9cbad6
Amukh1 <85003861+amukh1@users.noreply.github.com>
2024-11-23 03:33:09 +0000

    Temp commit images
    Co-authored-by: skhajanchi <skhajanchi@users.noreply.github.com>

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,10 +8,13 @@ import { useEffect, useState, useRef } from "react";
 import {Nav} from './Navbar'
 
 export function Home(props) {
-    return <div>
-        <title>Name</title>
-        <Nav/>
 
-        
+    return <div>
+        <title>Home | Vegan Villa</title>
+        <Nav/>
+        <div className="landing"></div>
+        {/* eee */}
+        {/* <img src="http://www.msn.com/en-us/foodanddrink/recipes/chicken-with-crushed-harissa-chickpeas/ar-BB1j9wiP?apiversion=v2&noservercache=1&domshim=1&renderwebcomponents=1&wcseo=1&batchservertelemetry=1&noservertelemetry=1" alt=""/> */}
+        {/* <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTgfjx-_exPt9OeXtAIkN6w4kWH7L7gzlOA7xGASCeoNtrtgaLQ" alt=""/> */}
     </div>
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,18 +9,12 @@ import { useEffect, useState, useRef, button } from "react";
 export function Nav(props) {
     return <div>
         <div className="bar">
-            <div className="bar1">
-                <div className="img">
-                    <img src="https://raw.githubusercontent.com/amukh1/WebmasterTSA/refs/heads/main/src/VV.png?token=GHSAT0AAAAAAC2QEKNEJB7SVJFKO66Y5FNQZZ6SAEA" alt="" />
-                
-                </div>
+                    
                 <p>Menu</p>
                 <p>About Us</p>
-            </div>
-            <div className="bar2">
-                <button>Location</button>
-                <button>Contact</button>
-            </div>
+                <img src="https://raw.githubusercontent.com/amukh1/WebmasterTSA/refs/heads/main/src/VV.png?token=GHSAT0AAAAAAC2NU56ROU6UL7RYY7DUFB46Z2BKDLQ" alt="" />
+                <p>Location</p>
+                <p>Contact</p>
         </div>
     </div>
 }
