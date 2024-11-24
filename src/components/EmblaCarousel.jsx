e621710544da19d8c55ac038d458211e3e093da6
amukh1 <fr0g_gam3r@outlook.com>
2024-11-23 19:51:38 -0500

    Imagepathupdate

--- src/components/EmblaCarousel.jsx
+++ src/components/EmblaCarousel.jsx
@@ -7,9 +7,9 @@ import {
 } from './EmblaCarouselArrowButtons'
 import useEmblaCarousel from 'embla-carousel-react'
 
-import food1 from '/Vegan_food.jpg'
-import food2 from '/vdp.webp'
-import food3 from '/img73872.whqc_2852x1426q80.webp'
+import food1 from 'https://ibb.co/Y7JRRQZ'
+import food2 from 'https://ibb.co/YWTYR5H'
+import food3 from 'https://ibb.co/HBjYK5z'
 
 const EmblaCarousel = (props) => {
   const { slides, options } = props
