4abce2effb24917450476247352df12afeb43ce5
Amukh1 <85003861+amukh1@users.noreply.github.com>
2025-01-09 02:26:24 +0000

    Commit

--- src/Reference.jsx
+++ src/Reference.jsx
@@ -4,6 +4,9 @@ import { ArcherContainer, ArcherElement } from "react-archer";
 
 import { Nav } from './Navbar';
 import { Footer } from './components/footer'
+import Work_Log from './Work_Log.pdf'
+import Student_Copyright_Checklist from './Student_Copyright_Checklist.pdf'
+import Citations from './Citations.pdf'
 
 export function Reference(props) {
     return <div>
@@ -12,45 +15,27 @@ export function Reference(props) {
         <div className="Reference">
 
 <br/>
+{/* <ul> */}
+<h1 className="menutitlee">References</h1>
 <ul>
-        <li><a href="https://hospitalityinsights.ehl.edu/vegan-philosophy-to-be-or-not-to-be" target="_blank">First Slideshow Picture</a></li>
-        <li><a href="https://www.greatbritishchefs.com/collections/vegan-dinner-party-recipes" target="_blank">Second Slideshow Picture</a></li>
-        <li><a href="https://www.greatbritishchefs.com/collections/vegan-recipes" target="_blank">Third Slideshow Picture</a></li>
-        <li><a href="https://www.google.com/maps/search/40.101763,+-79.365775?entry=tts&g_ep=EgoyMDI0MTExOS4yIPu8ASoASAFQAw%3D%3D" target="_blank">Google Maps Location</a></li>
-        <li><a href="https://www.reddit.com/r/Killtony/comments/1gxxbwy/guest_suggestion_for_the_monday_roast/" target="_blank">Gordon Ramsay Picture</a></li>
-        <li><a href="https://www.freepik.com/free-photo/front-view-portrait-businessman-with-glasses_11938203.htm" target="_blank">Picture of "Larry"</a></li>
-        <li><a href="https://hoopshype.com/2023/05/23/lebron-james-im-still-better-than-90-percent-of-the-nba-maybe-95/" target="_blank">LeBron James Picture</a></li>
-        <li><a href="https://www.greatbritishchefs.com/contributors/mildreds-vegan-cookbook" target="_blank">Vegan Raspberry, Almond, Cherry and Saffron Truffle Picture</a></li>
-        <li><a href="https://www.greatbritishchefs.com/recipes/vegan-creme-caramel-recipe" target="_blank">Vegan Espresso Crème Caramel With Pistachio Biscotti Picture</a></li>
-        <li><a href="https://www.greatbritishchefs.com/recipes/lavender-blackberry-sorbet-recipe" target="_blank">Lavender and Blackberry Sorbet Picture</a></li>
-        <li><a href="https://plantedinthekitchen.com/buffalo-cauliflower-wings/" target="_blank">Buffalo Cauliflower Wings Picture</a></li>
-        <li><a href="https://lynnecurry.com/artichoke-spinach-dip-recipe/" target="_blank">Spinach & Artichoke Dip Picture</a></li>
-        <li><a href="https://www.twopeasandtheirpod.com/category/recipes/page/19/" target="_blank">Zucchini Fritters Picture</a></li>
-        <li><a href="https://www.realcaliforniamilk.com/recipe/california-gouda-stuffed-mushrooms" target="_blank">Stuffed Mushrooms Picture</a></li>
-        <li><a href="https://www.pinterest.com/pin/168603579788905847/" target="_blank">Eggplant Parmesan Stacks Picture</a></li>
-        <li><a href="https://thefoodiephysician.com/chickpea-and-spinach-curry/" target="_blank">Chickpea and Spinach Curry Picture</a></li>
-        <li><a href="https://www.betternaturetempeh.co/recipe/teriyaki-tempeh" target="_blank">Tempeh Teriyaki Bowl Picture</a></li>
-        <li><a href="https://myfoodstory.com/fully-loaded-veggie-burger-with-avocado-mint-chutney-recipe/" target="_blank">Loaded Veggie Burger Picture</a></li>
-        <li><a href="https://newmansown.com/recipes/homestyle-lasagna/" target="_blank">Vegan Lasagna Picture</a></li>
-        <li><a href="https://daralamarmenu.fayssalbaccar.com/product/quinoa-salad/" target="_blank">Mediterranean Quinoa Salad</a></li>
-        <li><a href="https://www.purewow.com/food/vegan-side-dish-recipes" target="_blank">Roasted Veggie Salad</a></li>
-        <li><a href="https://www.bonappetit.com/recipe/citrus-and-avocado-salad-with-orange-water" target="_blank">Avocado & Citrus Salad Picture</a></li>
-        <li><a href="https://myquietkitchen.com/meyer-lemon-vegan-cheesecake/" target="_blank">Vegan Cheesecake Picture</a></li>
-        <li><a href="https://elegantdessertsny.com/double-chocolate-souffle-9x13/" target="_blank">Chocolate Lava Cake Picture</a></li>
-        <li><a href="https://in.pinterest.com/pin/160651911686974666/" target="_blank">Banana Nice Cream Sundae Picture</a></li>
-        <li><a href="https://www.buttercreamblondie.com/blogs/news/apple-cinnamon-crumb-cake" target="_blank">Apple Cinnamon Crumble Picture</a></li>
-        <li><a href="https://handletheheat.com/peanut-butter-stuffed-brownies/comment-page-10/?ssp_iabi=1683064607831" target="_blank">Peanut Butter Brownies Picture</a></li>
-        <li><a href="https://cookingwithcoit.com/dragon-fruit-smoothie/" target="_blank">Berry Bliss Smoothie Picture</a></li>
-        <li><a href="https://www.instagram.com/realandvibrant/p/B_X2-_ZndI9/" target="_blank">Golden Turmeric Latte Picture</a></li>
-        <li><a href="https://www.pinterest.com/pin/107734616062080171/" target="_blank">Iced Hibiscus Tea Picture</a></li>
-        <li><a href="https://www.romylondonuk.com/matcha-lemonade-starbucks/" target="_blank">Matcha Lemonade Picture</a></li>
-        <li><a href="https://www.rosemarywhisk.com/2024/05/toasted-coconut-shaken-espresso.html" target="_blank">Coconut Espresso Shake Picture</a></li>
-        <li><a href="https://www.craiyon.com/search/Large%20trucks%20filled%20with%20open%20boxes%20of%20ripe%20fruits%20and%20vegetables%20in%20a%20whimsical%20setting" target="_blank">Farm-to-table Process Picture</a></li>
-        <li><a href="https://www.youtube.com/watch?v=3MrH-Q1aCXE" target="_blank">Preparation Processes Picture</a></li>
-        <li><a href="https://www.freepik.com/premium-photo/eco-sustainable-development-sign-part-large-set-high-quality-eco-signs_18860013.htm" target="_blank">Our Approach to Sustainability Photo</a></li>
+        <li>
+            <a href={Citations} target="_blank">MLA Citations (see below)</a>
+        </li>
+        <li>
+            <a href={Work_Log} target="_blank">Work Log</a>
+        </li>
+        <li>
+            <a href={Student_Copyright_Checklist} target="_blank">Student Copyright Checklist</a>
+        </li>
     </ul>
+<div className="section toppies">
+        {/* <a href="https://github.com/amukh1/WebmasterTSA/blob/main/Citations.pdf">MLA Citations</a> */}
+
+        <object data={Citations} type="application/pdf" width="100%" height="100%">
+      <p>Work Log</p>
+        </object>
+</div>
     </div>
-    <br/>
     <Footer/>
     </div>
 }
